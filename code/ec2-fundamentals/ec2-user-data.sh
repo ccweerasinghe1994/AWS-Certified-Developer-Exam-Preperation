40cbd5cc0004d3b316eb385b638090852bc5730b
wchamara <chamara.weerasinghe@my.sliit.lk>
2024-03-08 08:33:46 +0530

    Update ec2-user-data.sh

--- code/ec2-fundamentals/ec2-user-data.sh
+++ code/ec2-fundamentals/ec2-user-data.sh
@@ -1,8 +1,13 @@
 #!/bin/bash
 # Use this for your user data (script from top to bottom)
 # install httpd (Linux 2 version)
+
+# Update the installed packages and package cache on your instance.
 yum update -y
+# Install the Apache web server.
 yum install -y httpd
+# Start the Apache web server.
 systemctl start httpd
 systemctl enable httpd
-echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html
+# Create a simple index.html file
+echo "<h1>Hello World from $(hostname -f)</h1>" > /var/www/html/index.html 
